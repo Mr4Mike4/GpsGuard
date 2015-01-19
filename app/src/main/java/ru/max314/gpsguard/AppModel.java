9fcb9b00d991f6fce5a8a72c621886cd44179b81
max <max314@mail.ru>
2015-01-19 07:58:53 +0300

    из дома

--- app/src/main/java/ru/max314/gpsguard/AppModel.java
+++ app/src/main/java/ru/max314/gpsguard/AppModel.java
@@ -1,11 +1,15 @@
 package ru.max314.gpsguard;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Handler;
 
 import java.util.Date;
-import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import ru.max314.util.threads.TimerHelper;
 
 
 /**
@@ -18,17 +22,41 @@ public class AppModel {
     private Date currentLocationDate;
     private Date currentLocationDateFix;
     private Date checkLocationDateFix;
-    LocationService locationService;
-    LocationFixService locationFixService;
+    private LocationService locationService;
+    private LocationFixService locationFixService;
+    private boolean isShowActivityOnTrouble = false;
+    private boolean isCloseActivityOnClick = false;
+
+    private Date verifyDate;
+    private String verifyString;
+
+
     Handler fixListenerStartHandler;
     Object guard = new Object();
 
-    private static void initModel() {
+    private static final String MODEL_PREF = "MODEL_PREF";
+    private static final String MODEL_SHOWACTIVITY = "MODEL_SHOWACTIVITY";
+    private static final String MODEL_CLOSEACTIVITY = "CLOSE_SHOWACTIVITY";
+
+    private synchronized static void initModel() {
         self = new AppModel();
+        SharedPreferences pref = App.getInstance().getSharedPreferences(MODEL_PREF, App.MODE_PRIVATE);
+        self.isShowActivityOnTrouble = pref.getBoolean(MODEL_SHOWACTIVITY, false);
+        self.isCloseActivityOnClick = pref.getBoolean(MODEL_CLOSEACTIVITY, false);
     }
 
     public AppModel() {
-
+        TimerHelper dateChangerWatcher = new TimerHelper("каждые пол минуты проверяем",
+                TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES), // Начинаем через миуту
+                TimeUnit.MILLISECONDS.convert(30, TimeUnit.SECONDS), // каждые полминуты
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        verify();
+                        showActivity();
+                    }
+                });
+        dateChangerWatcher.start();
     }
 
     public static synchronized AppModel getInstance() {
@@ -40,6 +68,42 @@ public class AppModel {
 
     public synchronized Location getCurrentLocation() {
         return currentLocation;
+    }
+
+    public synchronized Date getCurrentLocationDate() {
+        return currentLocationDate;
+    }
+
+    public synchronized Date getCurrentLocationDateFix() {
+        return currentLocationDateFix;
+    }
+
+    public synchronized Date getCheckLocationDateFix() {
+        return checkLocationDateFix;
+    }
+
+    public synchronized boolean isShowActivityOnTrouble() {
+        return isShowActivityOnTrouble;
+    }
+
+    public synchronized void setShowActivityOnTrouble(boolean isShowActivityOnTrouble) {
+        this.isShowActivityOnTrouble = isShowActivityOnTrouble;
+        SharedPreferences pref = App.getInstance().getSharedPreferences(MODEL_PREF, App.MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putBoolean(MODEL_SHOWACTIVITY, this.isShowActivityOnTrouble);
+        edit.commit();
+    }
+
+    public synchronized boolean isCloseActivityOnClick() {
+        return isCloseActivityOnClick;
+    }
+
+    public synchronized void setCloseActivityOnClick(boolean isCloseActivityOnClick) {
+        this.isCloseActivityOnClick = isCloseActivityOnClick;
+        SharedPreferences pref = App.getInstance().getSharedPreferences(MODEL_PREF, App.MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putBoolean(MODEL_CLOSEACTIVITY, this.isCloseActivityOnClick);
+        edit.commit();
     }
 
     public synchronized void setCurrentLocation(Object owner, Location currentLocation) {
@@ -71,6 +135,48 @@ public class AppModel {
         locationFixService = new LocationFixService((android.location.LocationManager) App.getInstance().getSystemService(Context.LOCATION_SERVICE));
         locationFixService.start();
 
+    }
+
+    /**
+     * Выполнить верификацию
+     */
+    public synchronized void verify() {
+        verifyDate = new Date();
+        verifyString = "";
+        // 1/ нет данных о местоположении
+        Date dt = getCurrentLocationDate();
+        if (dt==null){
+            verifyString = "Нет данных о местоположении";
+            return;
+        }
+        // не было обновления местоположения 5 сек
+        long delta = Math.abs(verifyDate.getTime() - dt.getTime());
+        long min = TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS);
+        if (delta>min){
+            verifyString = "Местоположение не получалось в течении 5 секунд";
+            return;
+        }
+        dt = getCheckLocationDateFix();
+        if (dt!=null)
+        {
+            // не было фикса за минуту
+            delta = Math.abs(verifyDate.getTime() - dt.getTime());
+            min = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES);
+            if (delta>min){
+                verifyString = "В течении минуты не было фиксации спутникоф";
+                return;
+            }
+        }
+    }
+
+    public void showActivity(){
+        if (verifyString!=null && verifyString.length()>0){
+            if (isShowActivityOnTrouble()){
+                Intent intent = new Intent(App.getInstance(),MainActivity.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+                App.getInstance().startActivity(intent);
+            }
+        }
     }
 
 }
--- app/src/main/java/ru/max314/gpsguard/MainActivity.java
+++ app/src/main/java/ru/max314/gpsguard/MainActivity.java
@@ -1,20 +1,119 @@
 package ru.max314.gpsguard;
 
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import ru.max314.util.threads.TimerUIHelper;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.tvDeviceTime)
+    TextView tvDeviceTime;
+    @InjectView(R.id.tvSpeed)
+    TextView tvSpeed;
+    @InjectView(R.id.tvCoord)
+    TextView tvCoord;
+    @InjectView(R.id.tvInfo)
+    TextView tvInfo;
+    @InjectView(R.id.cbShowActivity)
+    CheckBox cbShowActivity;
+    @InjectView(R.id.cbCloseOnClick)
+    CheckBox cbCloseOnClick;
+
+    TimerUIHelper timerUIHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        cbShowActivity.setChecked(AppModel.getInstance().isShowActivityOnTrouble());
+        cbCloseOnClick.setChecked(AppModel.getInstance().isCloseActivityOnClick());
+        cbShowActivity.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AppModel.getInstance().setShowActivityOnTrouble(isChecked);
+            }
+        });
+        cbCloseOnClick.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AppModel.getInstance().setCloseActivityOnClick(isChecked);
+            }
+        });
+
+
+        timerUIHelper = new TimerUIHelper(1000, new Runnable() {
+            @Override
+            public void run() {
+                updateData();
+            }
+        });
+    }
+
+    private void updateData(){
+        AppModel model = AppModel.getInstance();
+        Format format = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        String buff = format.format(date);
+        tvDeviceTime.setText(buff);
+        Location location = model.getCurrentLocation();
+        if (location==null){
+            tvSpeed.setText("0 м/с");
+            tvCoord.setText("");
+        }else {
+            tvSpeed.setText(String.format("%3f м/с",location.getSpeed()));
+            tvCoord.setText(String.format("%20.17f/%20.17f", location.getLatitude(), location.getLongitude()));
+        }
+        String pattern1="Месположение: <font color='blue'>%s</font> , фикс :<font color='blue'> %s</font> , проверочный фикс: <font color='blue'>%s</font> ";
+        buff = String.format(pattern1, formatDate(model.getCurrentLocationDate()),formatDate(model.getCurrentLocationDateFix()),formatDate(model.getCheckLocationDateFix()));
+        tvInfo.setText(Html.fromHtml(buff));
+    }
+
+    private String formatDate(Date value){
+        if (value == null) return "Нет";
+        Format format = new SimpleDateFormat("HH:mm:ss");
+        String buff = format.format(value);
+        return buff;
     }
 
 
+    //region OnClick
+    @OnClick(R.id.btClearAGPS)
+    public void onClearAGPSClick(){
+
+    }
+    @OnClick(R.id.btLoadAGPS)
+    public void onLoadAGPSClick(){
+
+    }
+
+    @OnClick(R.id.btHide)
+    public void onHideClick(){
+        this.finish();
+
+    }
+    //endregion
+
+    //region Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -33,7 +132,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+    //endregion
 }
